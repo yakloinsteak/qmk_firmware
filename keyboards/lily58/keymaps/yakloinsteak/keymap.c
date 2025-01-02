0bcff78f59fc7afed58b77af88a83bd412b4a504
Todd Blackman <tdblackman@gmail.com>
2025-01-02 03:20:49 +0000

    good

--- keyboards/lily58/keymaps/yakloinsteak/keymap.c
+++ keyboards/lily58/keymaps/yakloinsteak/keymap.c
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3     KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     YL_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     YL_ESC,   YL_A,   YL_S,    YL_D,    YL_F,    KC_G,                     KC_H,    YL_J,    YL_K,    YL_L,    YL_SCLN, TD(TD_QUOT),
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, YL_RSFT,
-                             YL_LTH4, YL_LTH3, KC_ENT, YL_LTH1, YL_RTH1, KC_SPC, YL_RTH3, YL_RTH4
+                             YL_LTH4, YL_LTH3, KC_ENT, YL_LTH1, YL_RTH1, KC_SPC, YL_RTH3, YL_RTH4,
 ),
 
 /* Tab hold
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [TAB_HOLD_LAYER] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     _______, _______, _______, _______, _______, _______,                   _______, KC_HOME, KC_PGUP, KC_INS,  KC_PSCR, KC_F12,
-    _______, _______, _______, KC_MINS, KC_GRV, _______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    _______, _______, _______, KC_MINS, KC_GRV,  _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, _______, _______, _______,
                                _______, _______, _______, _______, _______, _______, _______, _______
 ),
