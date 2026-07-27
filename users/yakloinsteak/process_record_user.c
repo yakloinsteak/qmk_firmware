4fd0488552aa67a03bae99fcbfa4947d874f9711
Todd Blackman <tdblackman@gmail.com>
2026-07-27 01:15:48 +0000

    try

--- users/yakloinsteak/process_record_user.c
+++ users/yakloinsteak/process_record_user.c
@@ -167,13 +167,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
 
     case KC_Q:
-        if ( (get_mods() & MOD_MASK_CTRL & MOD_MASK_GUI) && (record->event.pressed) ) {
+        if ( (get_mods() & (MOD_MASK_CTRL | MOD_MASK_GUI)) && record->event.pressed ) {
             register_code(KC_LALT);
             register_code(KC_LCTL);
-            SEND_STRING(SS_DELAY(2000));
+            SEND_STRING(SS_DELAY(1000));
             unregister_code(KC_LALT);
             unregister_code(KC_LCTL);
-            /* warp_mouse_move_px(0,-520); */
+            warp_mouse_move_px(0,-520);
             /* register_code(KC_RGUI); */
             /* register_code(KC_RCTL); */
 
