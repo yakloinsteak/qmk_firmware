8b1ede585c4b75f5331c70e81c7eaf34ef173e9d
Todd Blackman <tdblackman@gmail.com>
2026-07-30 20:22:11 +0000

    tune

--- users/yakloinsteak/process_record_user.c
+++ users/yakloinsteak/process_record_user.c
@@ -224,7 +224,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 case MON_LAPTOP: // the only screen
                     warp_mouse_to_screen(YL_SCREEN_LEFT);   warp_mouse_move_px(330, -220); break;
                 case MON_HOME:   // laptop is on the right; dialog opens on the left monitor
-                    warp_mouse_to_screen(YL_SCREEN_LEFT);   warp_mouse_move_px(380, -200); break; // TODO: tune
+                    warp_mouse_to_screen(YL_SCREEN_LEFT);   warp_mouse_move_px(400, -180); break; // TODO: tune
                 case MON_OFFICE: // center monitor
                     warp_mouse_to_screen(YL_SCREEN_CENTER); warp_mouse_move_px(500, -300); break; // TODO: tune
             }
